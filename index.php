9a345c345f8aef7ed4658debc9e29dd89385a1a9
Timo Pagel <github-commit@timo-pagel.de>
2022-03-13 11:56:02 +0100

    feat: add more debugging options

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
 $test=$_GET["test"];
-if(strpos($test, '..') !== false) {
-	echo "found '..'";
-	exit;
+$testfileDecoded=urldecode($test);
+if(strpos($test, '..') !== false) { 
+	echo "With simple path traversal detection I found '..'";
+} else {
+	echo "<hr></br>";
+	echo file_get_contents($testfileDecoded);
 }
-$testfileDecoded=urldecode($_GET['test']);
-echo "---</br>";
-echo file_get_contents($testfileDecoded);
+echo "<hr></br>";
+echo "Parameter testfileDecoded: $testfileDecoded";
